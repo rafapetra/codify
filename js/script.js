0e82d1c71a69a5a412ebfc636834fc6ff48dfdb0
Rafael Petrachini <raffp@me.com>
2022-06-19 10:45:14 -0700

    Add more branching to function's conditions.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// User Interface Logic
+// BUSINESS Logic
 
 //hiding the results
 function hideResults() {
@@ -8,6 +8,8 @@ function hideResults() {
 
 
 }
+
+// UI Logic
 
 //gather the data
 window.onload = function() {
@@ -19,15 +21,25 @@ window.onload = function() {
     const button1 = document.getElementById("option1");
     const button2 = document.getElementById("option2");
     const button3 = document.getElementById("option3");
+    const button4 = document.getElementById("option4");
+    const button8 = document.getElementById("option8");
+    const button11 = document.getElementById("option11");
+    const button15 = document.getElementById("option15");
 
 
 
-    if (button1.checked) {
+
+
+
+
+    if (button1.checked && button8.checked && button11.checked && button15.checked) {
       document.getElementById("swift").style.display = "block";
     }else if (button2.checked) {
       document.getElementById("phyton").style.display = "block";
     }else if (button3.checked) {
       document.getElementById("javascript").style.display = "block";
+    }else if (button1.checked) {
+      document.getElementById("swift").style.display = "block";
     }
 
 
